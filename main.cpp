04973efe36ee608a0dbe9ce6a174589efc332574
ZayK12 <27kazi-zayyaan@jonesboroschools.net>
2024-08-28 09:13:46 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -34,23 +34,15 @@ motor BackLeft = motor(PORT10, ratio18_1, false);
 
 motor FrontRight = motor(PORT11, ratio18_1, true);
 
-motor BackRight = motor(PORT2, ratio18_1, true);
+motor BackRight = motor(PORT2, ratio18_1, false);
 
 inertial Inertial4 = inertial(PORT4);
 
+motor MotorGroup18MotorA = motor(PORT18, ratio18_1, false);
+motor MotorGroup18MotorB = motor(PORT17, ratio18_1, false);
+motor_group MotorGroup18 = motor_group(MotorGroup18MotorA, MotorGroup18MotorB);
 
 
-
-
-
-
-
-
-
-
-
-motor_group leftside = motor_group(FrontLeft, BackLeft);
-motor_group rightside = motor_group(FrontRight, BackRight);
 
 
 // Helper to make playing sounds from the V5 in VEXcode easier and
@@ -71,18 +63,18 @@ bool RemoteControlCodeEnabled = true;
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
 /*    Author:       {author}                                                  */
-/*    Created:      {date}                                                    */
 /*    Description:  V5 project                                                */
+/*    Created:      {date}                                                    */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
 // Include the V5 Library
 #include "vex.h"
-int input = 1000;
-
+int input = 0;
+int turninput = 45;
 
 int TV = input/(3.14159 * 104.775) * 360;
-bool swtch = false; // switch to toggle while loop
+bool swtch = true; // switch to toggle while loop
 bool swtch2 = true; // debug switch
 
 
@@ -91,9 +83,9 @@ bool swtch2 = true; // debug switch
 using namespace vex;
 competition Competition;
 
-double Kp = 0.875; // Constant for Proportion
+double Kp = 0.275; // Constant for Proportion
 double Ki = 0.0001; // Constant for Integral
-double Kd = 0.00003;// Constant for Derivative
+double Kd = 0.0001;// Constant for Derivative
 float leftError = 0; //error is the distance left from the current position to the TV
 float leftIntegral = 0;
 float leftDerivative = 0;
@@ -105,10 +97,22 @@ float rightPrev_error = 0;
 float prev_error = 0; // This is just the previous error before the next loop
 float integral = 0; // 
 float derivative = 0; // error - prev_error : 
+float averagerot = 0; //
+
+
+double turnKp = 0.875; // Constant for Proportion
+double turnKi = 0.0001; // Constant for Integral
+double turnKd = 0.00003;// Constant for Derivative
+float turnError = 0; //error is the distance left from the current position to the TV
+float turnIntegral = 0;
+float turnDerivative = 0;
+float turnPrev_error = 0;
+float turnV = 0; 
 
 event getVar = event();
 
-int ondriver_drivercontrol_0() {
+int ondriver_drivercontrol_0() 
+{
   return 0;
 }
 
@@ -138,13 +142,24 @@ void onevent_getVar_1()
 {
   int FRMP = FrontRight.position(degrees);
   rightError = TV - FRMP;
-  
   rightIntegral += rightError;
   rightPrev_error = rightError;
-
 }
-void whenstarted1() 
+void onevent_getVar_2()
 {
+  int averagerot = (FrontRight.position(degrees) + FrontLeft.position(degrees)/2);
+  turnError = (turninput - averagerot) * -1;
+  
+  turnIntegral += turnError;
+  turnPrev_error = turnError;
+  turnV = turnKp*turnError + turnKi*turnIntegral + turnKd * (turnError - turnPrev_error);
+  if (turnV > float(10))
+  {
+    turnV = 10;
+  }
+}
+void whenstarted1()
+{ 
   while (swtch) 
   {
     float rightlateralMotorPower = ((Kp*rightError + Ki*rightIntegral + Kd * (rightError - rightPrev_error))/12);
@@ -164,11 +179,11 @@ void whenstarted1()
       //return 0;
     }
     
-    leftside.spin(forward, leftlateralMotorPower, voltageUnits::volt);
-    rightside.spin(forward, rightlateralMotorPower, voltageUnits::volt);
+    leftside.spin(forward, leftlateralMotorPower + turnV, voltageUnits::volt);
+    rightside.spin(forward, rightlateralMotorPower - turnV, voltageUnits::volt);
     //speed = Kp*error + Ki*integral + Kd * derivative;
     //return 0;
-  }
+  } 
 
 }
 
@@ -179,7 +194,7 @@ int whenStarted2()
     getVar.broadcast();
     Controller1.Screen.clearScreen();
     Controller1.Screen.setCursor(1,1);
-    Controller1.Screen.print(rightError);
+    Controller1.Screen.print(turnError);
   }
   return 0;
   
@@ -189,11 +204,11 @@ int whenStarted2()
 int main() {
   getVar(onevent_getVar_0);
   getVar(onevent_getVar_1);
+  getVar(onevent_getVar_2);
   vex::competition::bStopTasksBetweenModes = false;
   Competition.drivercontrol(VEXcode_driver_task);
   swtch = true;
   swtch2 = true;
   vex::task ws1(whenStarted2);
   whenstarted1();
-  
 }
